2faf87f318c2bd76df6f206fa36a2de95ecd9cc0
Kylee Willis <23221736+raspberrymilkyway@users.noreply.github.com>
2026-02-26 04:52:14 -0600

    skyscrapers cont.
    + import tmp, since that hasn't happened this project
    + make and rename arrows based on direction
    + add code to spawn tiles
    + add input field to duplicate during tile spawn

--- scratch_2d/Assets/Scripts/Skyscrapers.cs
+++ scratch_2d/Assets/Scripts/Skyscrapers.cs
@@ -9,11 +9,14 @@ public class Skyscrapers : MonoBehaviour
 
     public GameObject anchor;
     public GameObject sampleRow;
+    public GameObject sampleInputField;
 
     //i'm gonna try to randomize this eventually but.
     // i do think it'll need to be like. hand-done. for the game
     
     public int currentLevel = 1; //make private/protected internal in actual
+    public Color32 bgColor = new Color32(224, 224, 224, 255);
+    public Color32 arrowColor = new Color32(0, 0, 0, 255);
 
     [Header("Custom (unimplemented)")]
     public bool custom = false;
@@ -32,11 +35,12 @@ public class Skyscrapers : MonoBehaviour
     //oh this should maybe have a pencil option
 
     void Start(){
-        tileSize = SCREENSIZE / (buildingsPerRow+2);
+        tileSize = SCREENSIZE / (buildingsPerRow+4);
 
         if (!custom){
             loadFile();
         }
+        spawnTiles();
     }
 
     private void loadFile(){
@@ -48,6 +52,7 @@ public class Skyscrapers : MonoBehaviour
             lines[i] = lines[i].Trim('\r');
             string l = lines[i].Substring(2, lines[i].Length-4); //trim asterisk, space
             int[] arr = Array.ConvertAll(l.Split(" "), (item => Int32.Parse(item)));
+            buildingsPerRow = arr.Length;
 
             if (lines[i][0].Equals('*')){
                 if (i == 0){
@@ -68,41 +73,122 @@ public class Skyscrapers : MonoBehaviour
     }
 
     private void spawnTiles(){
-        spawnArrowRow();
-        for (int i=0; i<0; i++){
-            GameObject row = GameObjectUtility.DuplicateGameObject(sampleRow);
-            row.name = "row" + i;
-            row.transform.SetSiblingIndex(i+3);
-        }
+        //pascal case parameters, please!
+        spawnNumberRow("N");
+        spawnArrowRow("Down");
+        spawnSkyscrapers();
+        spawnArrowRow("Up");
+        spawnNumberRow("S");
         Destroy(sampleRow);
     }
 
-    private void spawnNumberRow(){
+    private void spawnNumberRow(string cardinal){
         GameObject row = GameObjectUtility.DuplicateGameObject(sampleRow);
-        row.name = "skyscraperCount";
-        row.transform.SetSiblingIndex(1);
-        for (int i=0; i<buildingsPerRow+1; i++){
-            GameObject arrow = new GameObject();
-            arrow.name = "arrow" + i;
-            Image arrowi = arrow.AddComponent<Image>();
-            arrow.GetComponent<RectTransform>().sizeDelta = new Vector2(tileSize, tileSize);
-            arrow.transform.SetParent(row.transform);
-            arrow.SetActive(true);
+        row.name = "numbers" + cardinal;
+        row.transform.SetSiblingIndex(anchor.transform.childCount);
+        for (int i=0; i<buildingsPerRow+4; i++){
+            GameObject number = new GameObject();
+            number.name = "number" + i;
+            Image numberi = number.AddComponent<Image>();
+            if (i==0 || i==1 || i==buildingsPerRow+2 || i==buildingsPerRow+3){
+                numberi.sprite = Resources.Load<Sprite>("skyscrapers/arrowNil");
+                numberi.color = bgColor;
+            }
+            else{
+                if (cardinal.Equals("N") || cardinal.Equals("North")){
+                    numberi.sprite = Resources.Load<Sprite>("skyscrapers/" + arrowsN[i-2].ToString());
+                }
+                else{
+                    numberi.sprite = Resources.Load<Sprite>("skyscrapers/" + arrowsS[i-2].ToString());
+                }
+            }
+            number.GetComponent<RectTransform>().sizeDelta = new Vector2(tileSize, tileSize);
+            number.transform.SetParent(row.transform);
+            number.SetActive(true);
         }
+        row.SetActive(true);
     }
 
-    private void spawnArrowRow(){
+    private void spawnArrowRow(string direction){
         GameObject row = GameObjectUtility.DuplicateGameObject(sampleRow);
-        row.name = "arrows";
-        row.transform.SetSiblingIndex(2);
-        for (int i=0; i<buildingsPerRow+1; i++){
+        row.name = "arrows" + direction;
+        row.transform.SetSiblingIndex(anchor.transform.childCount);
+        for (int i=0; i<buildingsPerRow+4; i++){
             GameObject arrow = new GameObject();
             arrow.name = "arrow" + i;
             Image arrowi = arrow.AddComponent<Image>();
+            if (i==0 || i==1 || i==buildingsPerRow+2 || i==buildingsPerRow+3){
+                arrowi.sprite = Resources.Load<Sprite>("skyscrapers/arrowNil");
+                arrowi.color = bgColor;
+            }
+            else{
+                arrowi.sprite = Resources.Load<Sprite>("skyscrapers/arrow" + direction);
+                arrowi.color = arrowColor;
+            }
             arrow.GetComponent<RectTransform>().sizeDelta = new Vector2(tileSize, tileSize);
             arrow.transform.SetParent(row.transform);
             arrow.SetActive(true);
         }
+        row.SetActive(true);
+    }
+
+    private void spawnSkyscrapers(){
+        for (int i=0; i<skyscrapers.Length; i++){
+            GameObject row = GameObjectUtility.DuplicateGameObject(sampleRow);
+            row.name = "scrapers" + i;
+            row.transform.SetSiblingIndex(anchor.transform.childCount);
+            for (int j=0; j<skyscrapers[i].Length+2; j++){
+                GameObject scrap = new GameObject();
+                scrap.name = "scrap" + j;
+                Image scrapi = scrap.AddComponent<Image>();
+                if (j==0){
+                    scrap.name = "numberW";
+                    scrap.transform.SetParent(row.transform);
+                    scrapi.sprite = Resources.Load<Sprite>("skyscrapers/" + arrowsW[i].ToString());
+                    GameObject arrow = new GameObject();
+                    arrow.name = "arrowRight";
+                    Image arrowi = arrow.AddComponent<Image>();
+                    arrowi.sprite = Resources.Load<Sprite>("skyscrapers/arrowRight");
+                    arrowi.color = arrowColor;
+                    arrow.GetComponent<RectTransform>().sizeDelta = new Vector2(tileSize, tileSize);
+                    arrow.transform.SetParent(row.transform);
+                    arrow.SetActive(true);
+                }
+                else if (j==skyscrapers[i].Length+1){
+                    scrapi.sprite = Resources.Load<Sprite>("skyscrapers/" + arrowsE[i].ToString());
+                    GameObject arrow = new GameObject();
+                    arrow.name = "arrowLeft";
+                    Image arrowi = arrow.AddComponent<Image>();
+                    arrowi.sprite = Resources.Load<Sprite>("skyscrapers/arrowLeft");
+                    arrowi.color = arrowColor;
+                    arrow.GetComponent<RectTransform>().sizeDelta = new Vector2(tileSize, tileSize);
+                    arrow.transform.SetParent(row.transform);
+                    arrow.SetActive(true);
+                    scrap.name = "numberE";
+                    scrap.transform.SetParent(row.transform);
+                }
+                else{
+                    scrap.transform.SetParent(row.transform);
+                    scrapText(scrap);
+                    scrapi.sprite = Resources.Load<Sprite>("skyscrapers/arrowNil");
+                    //...size is automatic(ally 0) until given a reason to exist.
+                    scrapi.color = new Color32(0, 0, 0, 0);
+                }
+                scrap.GetComponent<RectTransform>().sizeDelta = new Vector2(tileSize, tileSize);
+                scrap.SetActive(true);
+            }
+            row.SetActive(true);
+        }
+        Destroy(sampleInputField);
+    }
+
+    private void scrapText(GameObject scrap){
+        GameObject inp = GameObjectUtility.DuplicateGameObject(sampleInputField);
+        RectTransform rt = inp.GetComponent<RectTransform>();
+        rt.sizeDelta = new Vector2(tileSize, tileSize);
+        inp.transform.SetParent(scrap.transform);
+        rt.transform.localPosition = new Vector3(0,0,0);
+        inp.SetActive(true);
     }
 
     private void checkGrid(){
